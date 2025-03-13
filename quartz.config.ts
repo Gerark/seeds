45bebc3ca9b515a8e058e5df30e081861262dd71
Gerark <st4lk3r87@gmail.com>
2025-03-14 00:53:41 +0100

    Quartz sync: Mar 14, 2025, 12:53 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,8 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
-    pageTitleSuffix: "",
+    pageTitle: "A bunch of Seeds",
+    pageTitleSuffix: "Little Ideas and Learnings",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
